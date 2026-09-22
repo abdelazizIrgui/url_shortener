fd2dd457bbd7d0af6bed1b7c7ad2e3d13d4d2040
abdelaziz irgui <Azizcrypto382@gmail.com>
2026-09-22 18:31:05 +0000

    تحديث الكود

--- backend/server.js
+++ backend/server.js
@@ -185,7 +185,8 @@ process.on("uncaughtException", (err) => {
 db();
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`CORS allowed origin: ${allowedOrigins === true ? "all (dev mode)" : allowedOrigins.join(", ")}`);
 });
+
